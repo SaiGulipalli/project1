2ad29d323e865489a461d429272c7b01c14135a2
SaiGulipalli <84854470+SaiGulipalli@users.noreply.github.com>
2021-05-26 19:55:50 +0530

    Update Circle area.py

--- Circle area.py
+++ Circle area.py
@@ -1,4 +1,10 @@
-R = float(input("Enter the cirlcle's radius: ")
-pi= 3.1415926535
-area = pi*(r*r)
-ptint("Area of the circle with the radius ",R, "is:" ,area)
+R = float(input("Enter the cirlcle's radius: "))
+pi = 3.1415926535
+area = pi*(R*R)
+print("Area of the circle with the radius ",R, "is:" ,area)
+
+'''
+output 
+Enter the cirlcle's radius: 1.1
+Area of the circle with the radius  1.1 is: 3.801327110735001
+'''
